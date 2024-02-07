d7a3d5ce0eaa9c17a3269df31eaf2f68bd26a8ba
Aryan9901 <911aaryan@gmail.com>
2024-02-07 13:57:12 +0530

    images added in services section

--- Home Page/src/components/Services/Services.jsx
+++ Home Page/src/components/Services/Services.jsx
@@ -1,4 +1,6 @@
 import "./Services.css";
+import serviceimg from "../../assets/images/serviceimg.png";
+import threadimg from "../../assets/images/thread.png";
 function Services() {
 	return (
 		<div className="services">
@@ -6,6 +8,12 @@ function Services() {
 			<h2>Tailored IT services, limitless solutions.</h2>
 			<p>Transforming Challenges into solutions, our services redefine your digital experience.</p>
 			<button>See All Services</button>
+			<div>
+				<img src={serviceimg} alt="service image" />
+
+				<div></div>
+			</div>
+			<img src={threadimg} alt="thread" />
 		</div>
 	);
 }
