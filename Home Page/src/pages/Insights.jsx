58b8292b25d89e885107cd5822e4ca66fcaf3818
Raj Malpani <rajkm9111@gmail.com>
2024-02-09 15:37:42 +0530

    insight update

--- Home Page/src/pages/Insights.jsx
+++ Home Page/src/pages/Insights.jsx
@@ -4,12 +4,8 @@ import { BlogSection } from "../components";
 function Insights() {
 	return (
 		<>
-<<<<<<< HEAD
 			<Insight url="Home/Insights" heading="Insights" />
-=======
-			<Insight />
 			<BlogSection />
->>>>>>> 6d950378215b9f63491a9fc93ef6790d981d08fc
 		</>
 	);
 }
