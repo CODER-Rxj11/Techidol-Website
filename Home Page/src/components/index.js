d68d5e3dc2c4c52b48b5d2e579ac97ecba2e4014
Aryan9901 <911aaryan@gmail.com>
2024-02-09 16:00:22 +0530

    error commit

--- Home Page/src/components/index.js
+++ Home Page/src/components/index.js
@@ -4,9 +4,9 @@ import Footer from "./Footer/Footer";
 import Articles from "./Articles/Articles";
 import Promotion from "./Promotion/Promotion";
 import Subscription from "./Subscription/Subscription";
-import Testinomials from "./Testinomials/Testinomials";
+import Testinomial from "./Testinomial/Testinomial";
 import BlogComponent from "./BlogComponent/BlogComponent";
 import BlogSection from "./BlogSection/BlogSection";
 import Contact from "./Contact/Contact";
 
-export { Services, Timeline, Footer, Articles, Promotion, Subscription, Testinomials, BlogComponent, BlogSection, Contact };
+export { Services, Timeline, Footer, Articles, Promotion, Subscription, Testinomial, BlogComponent, BlogSection, Contact };
